855ec224bcdfc5619338502edf7534a5d6c42aa2
Maxime Bellec <maxbellec@gmail.com>
2021-07-19 09:56:45 +0200

    [*] cache page different for each user

--- white_rabbit/urls.py
+++ white_rabbit/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 from . import views
 from .views import MyLoginView
@@ -16,6 +17,6 @@ urlpatterns = [
         ),
     ),
     path("auth/", include("django.contrib.auth.urls")),
-    path("", cache_page(60)(views.HomeView.as_view())),
+    path("", cache_page(60)(vary_on_cookie(views.HomeView.as_view()))),
     path("", views.HomeView.as_view()),
 ]
