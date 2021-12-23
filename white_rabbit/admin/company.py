50eaeaa3487e311fa56c62b89d27c0bf764f5598
Maxime Bellec <maxbellec@gmail.com>
2021-12-23 14:41:56 +0100

    [+] company: add has_delete_permission

--- white_rabbit/admin/company.py
+++ white_rabbit/admin/company.py
@@ -9,3 +9,8 @@ class CompanyAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return Company.objects.all()
         return Company.objects.filter(admins=request.user)
+
+    def has_delete_permission(self, request, obj: Company = None):
+        if obj is None:
+            return True
+        return obj.admins.filter(pk=request.user.pk).exists()
